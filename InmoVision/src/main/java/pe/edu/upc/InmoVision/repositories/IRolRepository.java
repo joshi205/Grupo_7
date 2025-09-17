9e0e462193c8e59b507540bd75ee7c4c4f67ec8a
Nayeli Shirel Ramos Ulloa <142849336+joshi205@users.noreply.github.com>
2025-09-17 02:01:35 -0500

    Delete InmoVision directory

--- InmoVision/src/main/java/pe/edu/upc/InmoVision/repositories/IRolRepository.java
+++
@@ -1,9 +0,0 @@
-package pe.edu.upc.inmo.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import pe.edu.upc.inmo.entities.Rol;
-
-@Repository
-public interface IRolRepository extends JpaRepository<Rol,Integer> {
-}
