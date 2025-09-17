c67f0613338cd60e2ec8af2f9b193c3af5ea1fd1
Nayeli Shirel Ramos Ulloa <142849336+joshi205@users.noreply.github.com>
2025-09-17 02:02:41 -0500

    Add files via upload

---
+++ InmoVision/src/main/java/pe/edu/upc/InmoVision/InmoApplication.java
@@ -0,0 +1,13 @@
+package pe.edu.upc.inmo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InmoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InmoApplication.class, args);
+    }
+
+}
---
+++ InmoVision/src/main/java/pe/edu/upc/InmoVision/dtos/RolDTO.java
@@ -0,0 +1,31 @@
+package pe.edu.upc.inmo.dtos;
+
+public class RolDTO {
+    private int idRol;
+    private String nombreRol;
+    private String descripcionRol;
+
+    public int getIdRol() {
+        return idRol;
+    }
+
+    public void setIdRol(int idRol) {
+        this.idRol = idRol;
+    }
+
+    public String getNombreRol() {
+        return nombreRol;
+    }
+
+    public void setNombreRol(String nombreRol) {
+        this.nombreRol = nombreRol;
+    }
+
+    public String getDescripcionRol() {
+        return descripcionRol;
+    }
+
+    public void setDescripcionRol(String descripcionRol) {
+        this.descripcionRol = descripcionRol;
+    }
+}
